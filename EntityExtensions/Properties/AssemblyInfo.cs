7dbfe07c3ef6a10b96ad1c8f7612517d403d345e
Mahmoud Hanafy <Mahmoud.Hanafy@wolterskluwer.com>
2017-11-02 09:22:28 +1100

    Initial Nuget package

--- EntityExtensions/Properties/AssemblyInfo.cs
+++ EntityExtensions/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EntityExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Entity Framework extenstions library (Bulk processing and Meta helpers)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EntityExtensions")]
