60fdc99f7ac113cf441d2c10a324b4403334f1c3
Mahmoud Hanafy <Mahmoud.Hanafy@wolterskluwer.com>
2018-01-03 12:19:57 +1100

    remove comments

--- EntityExtensions.Tests/IntegrationTests.cs
+++ EntityExtensions.Tests/IntegrationTests.cs
@@ -267,33 +267,5 @@ namespace EntityExtensions.Tests
                 }
             }
         }
-
-        //[TestMethod]
-        //public void Test()
-        //{
-        //    using (var context = new CompanyContext())
-        //    {
-        //        var emps = context.Employees.ToList();
-        //        context.BulkUpdate(null, null, emps);
-
-        //        var emp1 = new Employee { Name = "Emp 01" };
-        //        var emp2 = new Employee { Name = "Emp 02" };
-        //        var emp3 = new Employee { Name = "Emp 03" };
-        //        var emp4 = new Employee { Name = "Emp 04" };
-        //        var emp5 = new Employee { Name = "Emp 05" };
-
-        //        emp2.Manager = emp1;
-        //        emp3.Manager = emp2;
-        //        emp4.Manager = emp3;
-        //        emp5.Manager = emp4;
-
-
-        //        emps = new List<Employee> { emp1, emp2, emp3, emp4, emp5 };
-        //        context.Employees.AddRange(emps);
-
-        //        context.BulkUpdate(emps);
-
-        //    }
-        //}
     }
 }
