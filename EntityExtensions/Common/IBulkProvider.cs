4bb43eca03d7e0d31024b5d5dc4a396ee7eb7c29
MHanafy <32828354+MHanafy@users.noreply.github.com>
2018-01-04 08:48:08 +1100

    Feature generic vendor support (#7)

---
+++ EntityExtensions/Common/IBulkProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace EntityExtensions.Common
+{
+    public interface IBulkProvider
+    {
+        /// <summary>
+        /// Writes the given datatable to the DB server using the given connection.
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="destTableName"></param>
+        /// <param name="data"></param>
+        void WriteToServer(DbConnection connection, string destTableName, DataTable data);
+    }
+}
